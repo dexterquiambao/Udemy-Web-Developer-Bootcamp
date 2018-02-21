dc881bb98c72070e3d163696e12d272d4142690d
Dexter Quiambao <dexterquiambao@googlemail.com>
2018-02-21 17:47:43 +0000

    Add files via upload

---
+++ jsExercise1.js
@@ -0,0 +1,11 @@
+var	firstName = prompt("What is your First Name?");
+var lastName = prompt("What is your Last Name?");
+var age = prompt("How old are you?");
+
+
+console.log("Your full Name is " + firstName + " " + lastName);
+console.log("You are " + age + " years old");
+
+// or use variable:
+// var fullName = firstName + " " + lastName);
+// then use console.log("Your full name is " + fullName);
---
+++ jsExercise2.js
@@ -0,0 +1,5 @@
+var age = prompt("How old are you?");
+alert("You have been alive for " + (age * 365.25) + " days");
+
+// or make a var days!
+// .25 accounts for leap year
