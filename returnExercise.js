bb61717b2ea32b14d3e3d8b6ed17dc2021f35399
Dexter Quiambao <dexterquiambao@googlemail.com>
2018-02-24 22:12:25 +0000

    Add files via upload

---
+++ returnExercise.js
@@ -0,0 +1,20 @@
+//isEven
+
+// function isEven(num){
+// 	if(num % 2 === 0) {
+// 		return true;
+// 	} else {
+// 		return false
+// 	}
+// }
+
+//return true if even
+//return false otherwise
+
+// or
+
+function isEven (num) {
+	return num % 2 === 0;
+}
+
+//this works because it is a true or false boolean
