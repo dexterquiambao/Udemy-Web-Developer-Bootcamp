e186ec903be05b934f9edef640e7439d45b27672
Dexter Quiambao <dexterquiambao@googlemail.com>
2018-03-01 17:24:28 +0000

    Update forLoopPractise.js

--- forLoopPractise.js
+++ forLoopPractise.js
@@ -3,3 +3,13 @@ let vacationSpots = ["California", "Toronto", "Paris"];
 for (let vacationSpotIndex = 0; vacationSpotIndex < vacationSpots.length; vacationSpotIndex++ ) {
   console.log ("I would love to visit " + vacationSpots[vacationSpotIndex]);
 }
+
+//forLoops backwards
+
+let vacationSpots = ["California", "Toronto", "Paris"];
+
+for (let vacationSpotIndex = vacationSpots.length - 1; vacationSpotIndex >= 0; vacationSpotIndex--) 
+{
+  //Because arrays start counting at 0, the start condition needs to be the length of the vacationSpots array minus 1.
+  console.log ("I would love to visit " + vacationSpots[vacationSpotIndex]);
+}
