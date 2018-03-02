1735ce02a22a7955d310ebd849e6213f79f9939d
Dexter Quiambao <dexterquiambao@googlemail.com>
2018-03-02 23:29:10 +0000

    Add files via upload

---
+++ arraysPractise.js
@@ -0,0 +1,14 @@
+let condiments = ['Ketchup', 'Mustard', 'Soy Sauce', 'Sriracha'];
+
+const utensils = ['Fork', 'Knife', 'Chopsticks', 'Spork'];
+
+condiments.push('Chilli Oil');
+console.log(condiments);
+
+condiments = ["chilli oil"];
+console.log(condiments);
+
+utensils.pop();
+console.log (utensils);
+
+utensils = ["spork"];
